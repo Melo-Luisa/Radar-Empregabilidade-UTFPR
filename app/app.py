4fc08408512fb69e30ef81e88e3d8cacd644a9e7
Luisa Melo <luhcotmel@gmail.com>
2026-02-18 16:07:12 -0300

    FEAT - inicializando o projeto

--- app/app.py
+++ app/app.py
@@ -1,2 +1,13 @@
-#teste de primeiro arquivo
-print("Hello, World!")
+
+import pandas as pd
+
+url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS5alLcu-PRBDOkUCAq-6UYTyB_I04abLzscaanJMAXem7zYN7YUf9sarwPXhMB9LhshaXpTBG13nJR/pub?output=csv"
+
+df = pd.read_csv(url)
+
+# print(df.head(30))
+# print(df.describe())
+# print(df.info())
+# print(df.shape)
+print(df.columns)
+
